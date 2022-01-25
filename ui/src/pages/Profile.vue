ab97ce51227e1362d5de9182b2f9d4fd43247590
Jost Schulte <jost.schulte@protonmail.com>
2022-01-25 12:30:26 +0100

    add saveProfile fn

--- ui/src/pages/Profile.vue
+++ ui/src/pages/Profile.vue
@@ -1,18 +1,20 @@
 <template>
     <q-page>
         <q-form class="q-mx-xl" style="max-width: 400px;">
-            <q-input v-model="profile.avatar" label="Avatar"/>
-            <q-input v-model="profile.bio" label="Bio"/>
-            <q-input v-model="profile.lang_pref" label="Language preference"/>
-            <q-input v-model="profile.location" label="Location"/>
+            <q-input v-model="profile.avatar" label="Avatar" />
+            <q-input v-model="profile.bio" label="Bio" />
+            <q-input v-model="profile.lang_pref" label="Language preference" />
+            <q-input v-model="profile.location" label="Location" />
+            <q-btn @click="saveProfile" icon="save">Save</q-btn>
         </q-form>
+        {{ errorMsg }}
     </q-page>
 </template>
 
 <script setup lang="ts">
 import { Profile } from 'ui/src/types/types';
-import {AppWebsocket} from '@holochain/client'
-import {onMounted, reactive, UnwrapRef} from 'vue'
+import { AppWebsocket, InstalledCell } from '@holochain/client'
+import { onMounted, reactive, ref } from 'vue'
 
 const profile = reactive<Profile>({
     avatar: '',
@@ -21,9 +23,17 @@ const profile = reactive<Profile>({
     location: ''
 })
 
+const errorMsg = ref('')
+
+let ws: AppWebsocket;
+let cell: InstalledCell
+let agentPubKey: Uint8Array;
+
 onMounted(async () => {
-    // const ws = await AppWebsocket.connect('ws://localhost:8888')
-    // const appInfo = await ws.appInfo({installed_app_id: 'test-app'})
+    ws = await AppWebsocket.connect(`ws://localhost:${import.meta.env.VITE_HC_PORT}`)
+    const appInfo = await ws.appInfo({ installed_app_id: 'test-app' })
+    cell = appInfo.cell_data[0]
+    agentPubKey = cell.cell_id[1]
     // const cell = appInfo.cell_data.find((cell) => cell.role_id === 'clutter');
     // if (!cell) {
     //     return
@@ -39,5 +49,18 @@ onMounted(async () => {
     // })
 })
 
-
+async function saveProfile() {
+    try {
+        await ws.callZome({
+            cell_id: cell.cell_id,
+            zome_name: 'profiles_stub',
+            fn_name: 'get_profile',
+            payload: {},
+            cap_secret: null,
+            provenance: agentPubKey
+        })
+    } catch (error) {
+        errorMsg.value = error instanceof Error ? error.message : String(error);
+    }
+}
 </script>
