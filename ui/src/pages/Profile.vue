2564e7422908a73c682fcc1e81887b93d6f1d356
Jost Schulte <jost.schulte@protonmail.com>
2022-01-26 22:27:57 +0100

    Move fn to create profile to hc-app-service

--- ui/src/pages/Profile.vue
+++ ui/src/pages/Profile.vue
@@ -16,7 +16,7 @@
 import { InstalledAppInfo } from '@holochain/client';
 import { Profile } from 'ui/src/types/types';
 import { onMounted, reactive, ref } from 'vue'
-import { useAppWebSocket } from '../services/hc-app';
+import { useAppWebSocket, createProfile } from '../services/hc-app-service';
 import { useQuasar } from 'quasar'
 const $q = useQuasar()
 
@@ -34,44 +34,17 @@ const appWs = useAppWebSocket();
 let appInfo: InstalledAppInfo
 
 onMounted(async () => {
-    appInfo = await appWs.appInfo({ installed_app_id: 'clutter' })
-    // const cell = appInfo.cell_data.find((cell) => cell.role_id === 'clutter');
-    // if (!cell) {
-    //     errorMsg.value = 'Could not find clutter cell'
-    //     return
-    // }
-    // const provenance = cell.cell_id[1]
-    // const result = await appWs.callZome({
-    //     cell_id: cell.cell_id,
+    // const profile = await loadProfile({
     //     zome_name: 'profiles_stub',
     //     fn_name: 'get_profile',
-    //     payload: {},
-    //     cap_secret: null,
-    //     provenance
     // })
 })
 
 const saveProfile = async () => {
-    if (!appInfo) {
-        errorMsg.value = 'Could not get app info'
-        return
-    }
-    const cell = appInfo.cell_data.find((cell) => cell.role_id === 'clutter');
-    if (!cell) {
-        errorMsg.value = 'Could not find clutter cell'
-        return;
-    }
-    const provenance = cell.cell_id[1]
     try {
-        const profilehash = await appWs.callZome({
-            cell_id: cell.cell_id,
-            zome_name: 'profiles_stub',
-            fn_name: 'create_profile',
-            payload: profile,
-            cap_secret: null,
-            provenance
-        })
-        $q.notify({message: 'Profile saved', color: 'green', position: 'bottom-right'})
+        const profilehash = await createProfile(profile)
+        console.log('profileHash', profilehash)
+        $q.notify({ message: 'Profile saved', color: 'green', position: 'bottom-right' })
     } catch (error) {
         errorMsg.value = error instanceof Error ? error.message : String(error);
     }
