b7c149ca467587e3c2806c4d0bd0fea386b8dae3
Jost Schulte <jost.schulte@protonmail.com>
2022-02-05 07:49:49 +0100

    add profiles route

--- ui/src/router.ts
+++ ui/src/router.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
     { path: "/", component: () => import("./pages/HomePage.vue") },
     { path: "/my-profile/", component: () => import("./pages/MyProfile.vue") },
-    { path: "/profile/:agent-pub-key", component: () => import("./pages/AgentProfile.vue") },
+    { path: "/profiles/:agent", component: () => import("./pages/AgentProfile.vue") },
     { path: "/feed", component: () => import("./pages/MewsFeed.vue") },
     { path: '/:pathMatch(.*)', component: () => import("./pages/NotFound.vue") }
 ];
