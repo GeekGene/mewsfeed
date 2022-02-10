855a894cea4961cacf2897a65b03d83fdce37262
Jost Schulte <jost.schulte@protonmail.com>
2022-02-10 21:44:10 +0100

    greet with display name

--- ui/src/pages/HomePage.vue
+++ ui/src/pages/HomePage.vue
@@ -3,10 +3,19 @@
     padding
     class="text-center"
   >
-    <h2>Discover more Clutter</h2>
+    <h6>Meeow, {{ displayName }}, you're in!</h6>
+    <h2>Welcome to the Clutter</h2>
     <q-img
       width="30%"
       src="/src/assets/img/cat-eating-bird-circle.png"
     />
   </q-page>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useProfileStore } from '../services/profile-store';
+const displayName = ref("");
+const store = useProfileStore();
+store.myProfile.subscribe((profile) => displayName.value = profile.fields["Display name"]);
+</script>
