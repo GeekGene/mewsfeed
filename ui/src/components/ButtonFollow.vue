3a69cca30eea77ab3af35a91ae83a3dcd07c18fe
Jost Schulte <jost.schulte@protonmail.com>
2022-02-12 10:31:42 +0100

    move follow btn to separate component

---
+++ ui/src/components/ButtonFollow.vue
@@ -0,0 +1,67 @@
+<template>
+  <q-btn
+    size="md"
+    color="accent"
+    @click="toggleFollow"
+  >
+    <template v-if="following">
+      <label class="q-mr-sm">Unfollow</label>
+      <q-icon
+        name="svguse:icons.svg#cat"
+        color=""
+      />
+    </template>
+    <template v-else>
+      <label class="q-mr-sm">Follow</label>
+      <q-icon
+        name="svguse:icons.svg#cat"
+        color="accent"
+        style="stroke: white;"
+      />
+    </template>
+  </q-btn>
+</template>
+
+<script setup lang="ts">
+import { HoloHashB64 } from '@holochain/client';
+import { follow, myFollowing, unfollow } from '../services/clutter-dna';
+import { showError, showMessage } from '../utils/notification';
+import { onMounted, PropType, ref } from 'vue';
+
+const props = defineProps({
+    agentPubKey: {
+        type: Object as PropType<HoloHashB64>,
+        required: true
+    }
+});
+
+const loading = ref(false);
+const following = ref(false);
+
+onMounted(async () => {
+    try {
+        loading.value = true;
+        const currentMyFollowing = await myFollowing();
+        following.value = currentMyFollowing.includes(props.agentPubKey);
+    } catch (error) {
+        showError(error);
+    } finally {
+        loading.value = false;
+    }
+});
+
+const toggleFollow = async () => {
+    try {
+        if (following.value) {
+            await unfollow(props.agentPubKey);
+        } else {
+            await follow(props.agentPubKey);
+        }
+        following.value = !following.value;
+        const message = following.value ? `You're following now` : 'Not following anymore';
+        showMessage(message);
+    } catch (error) {
+        showError(error);
+    }
+};
+</script>
--- ui/src/components/ProfilePopup.vue
+++ ui/src/components/ProfilePopup.vue
@@ -9,18 +9,23 @@
         <label>Nickname:</label>
         {{ nickname }}
       </div>
-      <q-icon
-        v-if="agentPubKey !== store.myAgentPubKey"
-        :name="following ? 'star' : 'star_outline'"
-        size="md"
-        color="accent"
-        class="cursor-pointer"
-        @click="toggleFollow"
+      <div class="q-mr-lg">
+        <label>Display name:</label>
+        {{ displayName }}
+      </div>
+      <ButtonFollow
+        :agent-pub-key="agentPubKey"
       />
     </q-card-section>
     <q-card-section>
-      <div><label>Bio: </label>{{ bio }}</div>
-      <div><label>Location: </label>{{ location }}</div>
+      <div>
+        <label>Bio:</label>
+        {{ bio }}
+      </div>
+      <div>
+        <label>Location:</label>
+        {{ location }}
+      </div>
     </q-card-section>
   </q-card>
 </template>
@@ -28,55 +33,39 @@
 <script setup lang="ts">
 import { HoloHashB64 } from '@holochain-open-dev/core-types';
 import { useProfileStore } from '../services/profile-store';
-import { showError, showMessage } from '../utils/notification';
-import { onMounted, PropType, ref } from 'vue';
-import { follow, myFollowing, unfollow } from '../services/clutter-dna';
+import { computed, onMounted, PropType, ref } from 'vue';
+import { showError } from '../utils/notification';
+import ButtonFollow from './ButtonFollow.vue';
 
+const profileStore = useProfileStore();
 const props = defineProps({
-    agentPubKey: {
-        type: Object as PropType<HoloHashB64>,
-        required: true
-    }
+  agentPubKey: {
+    type: Object as PropType<HoloHashB64>,
+    required: true
+  }
 });
+const isMyProfile = computed(() => props.agentPubKey === profileStore.myAgentPubKey);
 
-const loading = ref(false);
 const nickname = ref("");
+const displayName = ref("");
 const bio = ref("");
 const location = ref("");
-const following = ref(false);
-
-const store = useProfileStore();
+const loading = ref(false);
 
 onMounted(async () => {
-    try {
-        loading.value = true;
-        const profile = await store.fetchAgentProfile(props.agentPubKey);
-        if (profile) {
-            nickname.value = profile.nickname;
-            bio.value = profile.fields.Bio;
-            location.value = profile.fields.Location;
-        }
-        const currentMyFollowing = await myFollowing();
-        following.value = currentMyFollowing.includes(props.agentPubKey);
-    } catch (error) {
-        showError(error);
-    } finally {
-        loading.value = false;
+  try {
+    loading.value = true;
+    const profile = await profileStore.fetchAgentProfile(props.agentPubKey);
+    if (profile) {
+      nickname.value = profile.nickname;
+      displayName.value = profile.fields["Display name"];
+      bio.value = profile.fields.Bio;
+      location.value = profile.fields.Location;
     }
+  } catch (error) {
+    showError(error);
+  } finally {
+    loading.value = false;
+  }
 });
-
-const toggleFollow = async () => {
-    try {
-        if (following.value) {
-            await unfollow(props.agentPubKey);
-        } else {
-            await follow(props.agentPubKey);
-        }
-        following.value = !following.value;
-        const message = (following.value ? `You're following ` : 'You stopped following ') + nickname.value;
-        showMessage(message);
-    } catch (error) {
-        showError(error);
-    }
-};
 </script>
