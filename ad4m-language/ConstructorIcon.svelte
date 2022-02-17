dcbd5440e4adbd1f04ac7691a786ed96bc324474
Wesley Finck <wesleyfinck@gmail.com>
2022-02-17 15:16:25 -0800

    initial icon and added script to visually test icon

---
+++ ad4m-language/ConstructorIcon.svelte
@@ -0,0 +1,29 @@
+<svelte:options tag={null}/>
+
+<script lang="ts">
+    export let commitExpression
+    export let discard
+
+    let text = ""
+
+</script>
+
+<div class="container">
+    <input bind:value={text}>
+    <button on:click={()=>commitExpression(text)}>Commit</button>
+    <button on:click={discard}>Discard</button>
+</div>
+
+
+<style>
+    .container {
+        color: burlywood;
+        width: 400px;
+        height: 300px;
+    }
+
+    input {
+        width: 100%;
+        height: 200px;
+    }
+</style>
---
+++ ad4m-language/Icon.svelte
@@ -0,0 +1,13 @@
+<svelte:options tag={null}/>
+
+<script lang="ts">
+</script>
+
+<div class="container">
+    <p>Hey there</p>
+</div>
+
+
+<style>
+
+</style>
