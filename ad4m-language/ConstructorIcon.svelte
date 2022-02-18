df6f49b7c354f54a4a03d0ccf50251e66d10593a
Wesley Finck <wesleyfinck@gmail.com>
2022-02-18 13:19:19 -0800

    WIP: basic mew icon (still needs to interact with ad4m

--- ad4m-language/ConstructorIcon.svelte
+++ ad4m-language/ConstructorIcon.svelte
@@ -2,15 +2,28 @@
 
 <script lang="ts">
     export let commitExpression
+    export let mewType
     export let discard
 
     let text = ""
+    if (!mewType) {
+        mewType = {
+            original: null
+        }
+    }
+    function createMewInput(text) {
+        let createMewInput = {
+            mewType: mewType,
+            mew: text
+        }
+        return createMewInput
+    }
 
 </script>
 
 <div class="container">
     <input bind:value={text}>
-    <button on:click={()=>commitExpression(text)}>Commit</button>
+    <button on:click={()=>commitExpression(createMewInput(text))}>Publish Mew</button>
     <button on:click={discard}>Discard</button>
 </div>
 
--- ad4m-language/Icon.svelte
+++ ad4m-language/Icon.svelte
@@ -1,13 +1,77 @@
 <svelte:options tag={null}/>
 
 <script lang="ts">
+    // import type Expression from "@perspect3vism/ad4m/Expression";
+    import ConstructorIcon from "./ConstructorIcon.svelte";
+    export let expression//: Expression
+    let feedMewWithContext
+    $: if(expression && expression.data) feedMewWithContext = JSON.parse(expression.data)
+    let isReplying = false
+    let isMewMewing = false
+    function toggleLikeMew() {
+        //TODO: create function in adapter for liking/unliking mews
+        console.log(feedMewWithContext.mewEntryHash)
+    }
+    function replyToMew() {
+        if (isMewMewing) {
+            isMewMewing = !isMewMewing
+        }
+        isReplying = !isReplying
+    }
+    function shareMew() {
+        const createMewInput = {
+            mewType: {
+                reMew: feedMewWithContext.mewEntryHash
+            },
+            mew: null
+        }
+        createExpression(createMewInput)
+    }
+    function mewMew() {
+        if (isReplying) {
+            isReplying = !isReplying
+        }
+        isMewMewing = !isMewMewing
+    }
+
+    function createExpression(createMewInput) {
+        console.log(createMewInput)
+        //TODO: call createPublic in adapter
+    }
 </script>
 
 <div class="container">
-    <p>Hey there</p>
+    <div class="avatar-box"></div>
+    <div class="mew-meta">
+        {expression.author} {expression.timestamp}
+    </div>
+    <div class="mew-content">
+        <p>{feedMewWithContext.feedMew.mew.mew.mew}</p>
+    </div>
+    <div class="mew-interactions">
+        <button on:click={() => toggleLikeMew()}>like ({feedMewWithContext.likes.length})</button>
+        <button on:click={() => replyToMew()}>reply ({feedMewWithContext.comments.length})</button>
+        <button on:click={() => shareMew()}>share ({feedMewWithContext.shares.length})</button>
+        <button on:click={() => mewMew()}>mewmew ({feedMewWithContext.shares.length})</button>
+    </div>
+    {#if isReplying}
+        <ConstructorIcon commitExpression={createExpression} mewType={{reply:feedMewWithContext.mewEntryHash}}/>
+    {:else if isMewMewing}
+        <ConstructorIcon commitExpression={createExpression} mewType={{mewMew:feedMewWithContext.mewEntryHash}}/>
+    {/if}
 </div>
 
 
 <style>
-
+    .container {
+        width: 400px;
+        height: 300px;
+        background-color: lightblue;
+    }
+    .avatar-box {
+        width: 50px;
+        height: 50px;
+        border-radius: 50px;
+        background: blue;
+    }
 </style>
