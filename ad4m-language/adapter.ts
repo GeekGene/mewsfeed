c1bff41735ad0b749c14fa455f072f064c8414e4
Wesley Finck <wesleyfinck@gmail.com>
2022-03-10 15:30:04 -0800

    add agent key and timestamp from mew header to ad4m expression metadata

--- ad4m-language/adapter.ts
+++ ad4m-language/adapter.ts
@@ -1,6 +1,7 @@
 import type { Address, Agent, Expression, PublicSharing, LanguageContext, HolochainLanguageDelegate, ExpressionAdapter, AgentService } from "@perspect3vism/ad4m";
 import { DNA_NICK } from "./dna";
 import { serializeHash } from "@holochain-open-dev/core-types";
+import { Base64 } from 'js-base64';
 
 export default class ExpressionAdapterImpl implements ExpressionAdapter {
   #DNA: HolochainLanguageDelegate;
@@ -20,9 +21,8 @@ export default class ExpressionAdapterImpl implements ExpressionAdapter {
       "get_feed_mew_and_context",
       mewAddress
     );
-    const mew = JSON.parse(fullMewWithContext)
-    const author = serializeHash(new Uint8Array(mew.header.author.data)) 
-    const timestamp = mew.header.timestamp
+    const author = serializeHash(new Uint8Array(fullMewWithContext.feedMew.header.author.data)) 
+    const timestamp = fullMewWithContext.feedMew.header.timestamp
     return {
       author: author,
       timestamp: timestamp,
