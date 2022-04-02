72215ffb8abe7e534ec419634fefa3ee8918d37b
Jost Schulte <jost.schulte@protonmail.com>
2022-04-02 12:13:50 -0600

    highlight hashtags in mews

--- ui/src/components/MewsItemContent.vue
+++ ui/src/components/MewsItemContent.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="column">
-    <div 
-      v-if="mewContent.mew.mew"
+    <div
       class="text-body1"
       style="overflow-wrap: anywhere;"
     >
-      {{ mewContent.mew.mew.mew }}
+      <div v-html="content" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { FeedMew } from "@/types/types";
-import { PropType } from "vue";
-defineProps({ mewContent: { type: Object as PropType<FeedMew>, required: true } });
+import { PropType, ref } from "vue";
+const props = defineProps({ mewContent: { type: Object as PropType<FeedMew>, required: true } });
+
+const content = ref(props.mewContent.mew.mew?.mew || '');
+const hashtagRegex = /#(\w+)/g;
+const matches = [...content.value.matchAll(hashtagRegex)];
+matches.forEach((captureGroup) => {
+  const [hashtag, match] = captureGroup;
+  content.value = content.value.replaceAll(hashtag, `<a href="/feed/hashtag/${match}">#${match}</a>`);
+});
 </script>
