bf616c73a975537dc1fbc68826a9e4286cce28a5
Jost Schulte <jost.schulte@protonmail.com>
2022-05-02 20:31:29 -0600

    feat(nav): add mew btn

--- ui/src/layouts/MainLayout.vue
+++ ui/src/layouts/MainLayout.vue
@@ -1,13 +1,13 @@
 <template>
-  <q-layout view="hHh lpr lfr">
+  <q-layout view="hHh lpR fFf">
     <q-header elevated class="row justify-center">
-      <q-toolbar class="col col-sm-6 col-lg-5">
-        <q-tabs v-model="tab" dense inline-label>
+      <q-toolbar class="col-12 col-md-6">
+        <q-tabs v-model="tab" dense inline-label class="col-grow">
           <q-route-tab to="/">
             <q-icon name="svguse:/icons.svg#cat" size="lg" />
           </q-route-tab>
 
-          <q-space />
+          <q-btn icon="add" color="secondary" class="q-mx-md">Mew</q-btn>
 
           <q-select
             v-model="selection"
@@ -15,25 +15,24 @@
             :loading="searching"
             input-debounce="0"
             label="Sniff around"
-            label-color="secondary"
             behavior="menu"
             standout
             use-input
             hide-dropdown-icon
             hide-selected
             dark
+            class="col q-mx-md"
             :options-dark="false"
             dense
-            class="col-4 bg-white"
             @filter="search"
             @update:model-value="onAgentSelect"
           >
             <template #prepend>
-              <q-icon name="search" color="secondary" />
+              <q-icon name="search" />
             </template>
             <template #no-option>
               <q-item>
-                <q-item-section>
+                <q-item-section class="text-grey">
                   {{
                     searchTerm.length < 3
                       ? "Minimum 3 characters required"
@@ -44,28 +43,19 @@
             </template>
           </q-select>
 
-          <q-space />
-
-          <q-route-tab to="/feed" icon="feed" label="Mews" />
-
-          <q-space />
+          <q-route-tab to="/feed" icon="feed" label="Mews Feed" />
 
           <q-route-tab to="/my-profile">
             <agent-avatar :agent-pub-key="store.myAgentPubKey" size="40" />
-            <div class="lg q-ml-sm text-weight-bold">Profile</div>
           </q-route-tab>
         </q-tabs>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" side="left">
-      <sidebar-left />
-    </q-drawer>
-
-    <q-drawer v-model="rightDrawerOpen" side="right" />
-
-    <q-page-container>
-      <router-view />
+    <q-page-container class="row">
+      <q-space />
+      <router-view class="col-12 col-md-6" />
+      <q-space />
     </q-page-container>
   </q-layout>
 </template>
@@ -76,13 +66,10 @@ import { QSelectOption } from "quasar";
 import { useRouter } from "vue-router";
 import { ref } from "vue";
 import { showError } from "@/utils/notification";
-import SidebarLeft from "@/components/SidebarLeft.vue";
 
 const store = useProfileStore();
 const router = useRouter();
 const tab = ref("");
-const leftDrawerOpen = ref(true);
-const rightDrawerOpen = ref(true);
 
 const searching = ref(false);
 const options = ref<QSelectOption[]>([]);
