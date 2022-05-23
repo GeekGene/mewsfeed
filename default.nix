ebbd886e46e7868f1fc25ac82088dbc553e87076
Jost Schulte <jost.schulte@protonmail.com>
2022-05-22 19:22:22 -0600

    build(hc): upgrade to hc 0.0.139 & hdk 0.132

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
   holonixPath = (import ./nix/sources.nix).holonix;
   holonix = import (holonixPath) {
-    holochainVersionId = "v0_0_127";
+    holochainVersionId = "v0_0_139";
   };
   nixpkgs = holonix.pkgs;
 in
