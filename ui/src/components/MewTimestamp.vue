e13ad58bf5d488f310eb17fa1f242f61f36c2f3c
Matt Gabrenya <matt@buildyourweb.app>
2023-01-09 15:27:32 -0700

    feat: humanize mew timestamps

--- ui/src/components/MewTimestamp.vue
+++ ui/src/components/MewTimestamp.vue
@@ -4,26 +4,12 @@
 
 <script setup lang="ts">
 import { computed } from "vue";
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 const props = defineProps({ timestamp: { type: Number, required: true } });
 
 const timestamp = computed(() => new Date(Number(props.timestamp) / 1000));
-const dateFormatOptions: Intl.DateTimeFormatOptions = {
-  year: "numeric",
-  month: "short",
-  day: "2-digit",
-};
-const timeFormatOptions: Intl.DateTimeFormatOptions = {
-  hour: "numeric",
-  minute: "numeric",
-};
-const formattedDate = computed(() =>
-  new Intl.DateTimeFormat("default", dateFormatOptions).format(timestamp.value)
-);
-const formattedTime = computed(() =>
-  new Intl.DateTimeFormat("default", timeFormatOptions).format(timestamp.value)
-);
 const formattedTimestamp = computed(
-  () => `${formattedTime.value} on ${formattedDate.value}`
+  () => formatDistanceToNow(timestamp.value, {addSuffix: true})
 );
 </script>
