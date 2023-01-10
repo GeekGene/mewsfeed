b56b181b330892acf17ed596619fb391a0e5c25b
Matt Gabrenya <matt@buildyourweb.app>
2023-01-10 10:07:20 -0700

    feat: support multi-word url tags with format ^[link text]

--- ui/src/utils/tags.ts
+++ ui/src/utils/tags.ts
@@ -9,6 +9,7 @@ const regexpString = [
   `\\${TAG_SYMBOLS.CASHTAG}\\w+`,
   `\\${TAG_SYMBOLS.HASHTAG}\\w+`,
   `\\${TAG_SYMBOLS.MENTION}\\S+`,
-  `\\${TAG_SYMBOLS.URL}\\S+`,
+  `\\${TAG_SYMBOLS.URL}\\[[\\w ]+\\]`, // multi-word url
+  `\\${TAG_SYMBOLS.URL}\\S+`, // single-word url
 ];
 export const TAG_REGEX = new RegExp(`\\B(${regexpString.join("|")})`, "gi");
