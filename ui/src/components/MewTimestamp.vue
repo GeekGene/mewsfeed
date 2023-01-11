c3a77d132e2d276633612a7083f5d57cc8a8f778
Matt Gabrenya <matt@buildyourweb.app>
2023-01-11 13:21:46 -0700

    refactor: replace date-fns with dayjs to reduce bundle size

--- ui/src/components/MewTimestamp.vue
+++ ui/src/components/MewTimestamp.vue
@@ -4,12 +4,13 @@
 
 <script setup lang="ts">
 import { computed } from "vue";
-import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
 
 const props = defineProps({ timestamp: { type: Number, required: true } });
 
 const timestamp = computed(() => new Date(Number(props.timestamp) / 1000));
-const formattedTimestamp = computed(
-  () => formatDistanceToNow(timestamp.value, {addSuffix: true})
-);
+const formattedTimestamp = computed(() => dayjs().to(dayjs(timestamp.value)));
 </script>
