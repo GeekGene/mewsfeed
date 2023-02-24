f131ea320d2470aa84158930a786be38371a664b
Matt Gabrenya <matt@buildyourweb.app>
2023-02-23 18:36:09 -0700

    fix(ui): prevent inconsistently misidentifying tags

--- ui/src/utils/tags.ts
+++ ui/src/utils/tags.ts
@@ -13,24 +13,21 @@ const regexpString = [
   `\\B\\${TAG_SYMBOLS.URL}\\S+`, // single-word labeled url
 ];
 
-export const TAG_REGEX = new RegExp(`(${regexpString.join("|")})`, "gi");
+export const TAG_REGEX = new RegExp(`${regexpString.join("|")}`, "mi");
 
 export const RAW_URL_REGEX =
   /\b(?:(?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()[\]{};:'".,<>?«»“”‘’]))/;
 
 export const TAG_OR_RAW_URL_REGEX = new RegExp(
   `(${[...regexpString, RAW_URL_REGEX.source].join("|")})`,
-  "gmi"
+  "mi"
 );
 
 export const isTag = (text: string): boolean => TAG_REGEX.test(text);
 
 export const isRawUrl = (text: string): boolean => RAW_URL_REGEX.test(text);
 
-export const splitMewTextIntoParts = (text: string): string[] => {
-  const result = text
+export const splitMewTextIntoParts = (text: string): string[] =>
+  text
     .split(TAG_OR_RAW_URL_REGEX)
-    .filter((part) => part !== undefined);
-
-  return result;
-};
+    .filter((part) => part !== undefined && part.length > 0);
