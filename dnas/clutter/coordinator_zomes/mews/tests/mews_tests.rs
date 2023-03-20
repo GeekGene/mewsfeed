3c089b2bea7809efec2981c354fe028722372057
Harlan Wood <harlan.wood@sharespost.com>
2023-03-19 14:29:22 -1000

    Sweet!

--- dnas/clutter/coordinator_zomes/mews/tests/mews_tests.rs
+++ dnas/clutter/coordinator_zomes/mews/tests/mews_tests.rs
@@ -12,67 +12,15 @@ use mews_integrity::*;
 
 const DNA_FILEPATH: &str = "../../workdir/clutter.dna";
 
-// TESTING UTILITY FUNCTIONS
-
-async fn setup_1_conductor() -> (SweetConductor, AgentPubKey, SweetCell) {
-    let dna = SweetDnaFile::from_bundle(std::path::Path::new(DNA_FILEPATH))
-        .await
-        .unwrap();
-
-    let mut conductor = SweetConductor::from_standard_config().await;
-
-    let holo_core_agent = SweetAgents::one(conductor.keystore()).await;
-    let app1 = conductor
-        .setup_app_for_agent("app", holo_core_agent.clone(), &[dna.clone()])
-        .await
-        .unwrap();
-
-    let cell1 = app1.into_cells()[0].clone();
-
-    let agent_hash = holo_core_agent.into_inner();
-    let agent = AgentPubKey::from_raw_39(agent_hash).unwrap();
-
-    (conductor, agent, cell1)
-}
-
-pub async fn setup_conductors(n: usize) -> (SweetConductorBatch, Vec<AgentPubKey>, SweetAppBatch) {
-    let dna = SweetDnaFile::from_bundle(std::path::Path::new(DNA_FILEPATH))
-        .await
-        .unwrap();
-
-    let mut conductors = SweetConductorBatch::from_standard_config(n).await;
-
-    let all_agents1: Vec<holochain::core::AgentPubKey> =
-        future::join_all(conductors.iter().map(|c| SweetAgents::one(c.keystore()))).await;
-
-    let all_agents2: Vec<AgentPubKey> = all_agents1
-        .iter()
-        .map(|holo_core_agent| {
-            let agent_hash = holo_core_agent.clone().into_inner();
-            AgentPubKey::from_raw_39(agent_hash).unwrap()
-        })
-        .collect();
-
-    let apps = conductors
-        .setup_app_for_zipped_agents("app", &all_agents1, &[dna])
-        .await
-        .unwrap();
-
-    conductors.exchange_peer_info().await;
-    (conductors, all_agents2, apps)
-}
-
 // TESTS:
 
 #[tokio::test(flavor = "multi_thread")]
-#[should_panic]
-pub async fn mew_must_not_be_longer_than_200_chars() {
-    // try {
+pub async fn mew_can_be_200_chars() {
     let (conductor, alice, cell1): (SweetConductor, AgentPubKey, SweetCell) =
         setup_1_conductor().await;
 
     let long_mew = std::iter::repeat('a').take(200).collect::<String>();
-    let mut createMewInput = CreateMewInput {
+    let createMewInput = CreateMewInput {
         text: Some(long_mew),
         mew_type: MewType::Original,
         links: None,
@@ -85,19 +33,24 @@ pub async fn mew_must_not_be_longer_than_200_chars() {
     let bytes = mewHash.get_raw_39();
     let leading_bytes = bytes.get(..3).unwrap();
     assert_eq!(leading_bytes, &[132, 41, 36]);
+}
+
+#[should_panic(expected = "CellError")]
+#[tokio::test(flavor = "multi_thread")]
+pub async fn mew_must_not_be_longer_than_200_chars() {
+    let (conductor, alice, cell1): (SweetConductor, AgentPubKey, SweetCell) =
+        setup_1_conductor().await;
 
     let too_long_mew = std::iter::repeat('a').take(201).collect::<String>();
-    createMewInput = CreateMewInput {
+    let createMewInput = CreateMewInput {
         text: Some(too_long_mew),
         mew_type: MewType::Original,
         links: None,
     };
 
-    // assert_panics!("Expected panic did not occur", {
-    let x: () = conductor
+    let _: () = conductor
         .call(&cell1.zome("mews"), "create_mew", createMewInput)
         .await;
-    // });
 }
 
 // #[tokio::test(flavor = "multi_thread")]
@@ -1003,3 +956,53 @@ pub async fn mew_must_not_be_longer_than_200_chars() {
 //   );
 
 // });
+
+// TESTING UTILITY FUNCTIONS
+
+async fn setup_1_conductor() -> (SweetConductor, AgentPubKey, SweetCell) {
+    let dna = SweetDnaFile::from_bundle(std::path::Path::new(DNA_FILEPATH))
+        .await
+        .unwrap();
+
+    let mut conductor = SweetConductor::from_standard_config().await;
+
+    let holo_core_agent = SweetAgents::one(conductor.keystore()).await;
+    let app1 = conductor
+        .setup_app_for_agent("app", holo_core_agent.clone(), &[dna.clone()])
+        .await
+        .unwrap();
+
+    let cell1 = app1.into_cells()[0].clone();
+
+    let agent_hash = holo_core_agent.into_inner();
+    let agent = AgentPubKey::from_raw_39(agent_hash).unwrap();
+
+    (conductor, agent, cell1)
+}
+
+pub async fn setup_conductors(n: usize) -> (SweetConductorBatch, Vec<AgentPubKey>, SweetAppBatch) {
+    let dna = SweetDnaFile::from_bundle(std::path::Path::new(DNA_FILEPATH))
+        .await
+        .unwrap();
+
+    let mut conductors = SweetConductorBatch::from_standard_config(n).await;
+
+    let all_agents1: Vec<holochain::core::AgentPubKey> =
+        future::join_all(conductors.iter().map(|c| SweetAgents::one(c.keystore()))).await;
+
+    let all_agents2: Vec<AgentPubKey> = all_agents1
+        .iter()
+        .map(|holo_core_agent| {
+            let agent_hash = holo_core_agent.clone().into_inner();
+            AgentPubKey::from_raw_39(agent_hash).unwrap()
+        })
+        .collect();
+
+    let apps = conductors
+        .setup_app_for_zipped_agents("app", &all_agents1, &[dna])
+        .await
+        .unwrap();
+
+    conductors.exchange_peer_info().await;
+    (conductors, all_agents2, apps)
+}
