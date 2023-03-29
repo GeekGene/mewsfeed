42c3b0a2092e294c5593b4c546ffd4037e748f8b
Matt Gabrenya <matt@buildyourweb.app>
2023-03-29 11:48:04 -0700

    refactor(ui): extract logic to run a function after profile has been created into helper util

--- ui/src/utils/profile.ts
+++ ui/src/utils/profile.ts
@@ -1,6 +1,11 @@
 import { ROUTES } from "@/router";
 import { AgentPubKey, encodeHashToBase64 } from "@holochain/client";
 import { useRoute, useRouter } from "vue-router";
+import { onUnmounted, ref } from "vue";
+import { useProfilesStore } from "@/services/profiles-store";
+import { Profile } from "@holochain-open-dev/profiles";
+import { useQuasar } from "quasar";
+import CreateProfileDialog from "@/components/CreateProfileDialog.vue";
 
 export const useProfileUtils = () => {
   const route = useRoute();
@@ -19,4 +24,32 @@ export const useProfileUtils = () => {
   };
 
   return { isCurrentProfile, onAgentClick };
+};
+
+export const useMyProfile = () => {
+  const profilesStore = useProfilesStore();
+  const $q = useQuasar();
+
+  const myProfile = ref<undefined | Profile>(undefined);
+
+  const unsubscribe = profilesStore.value.myProfile.subscribe((res: any) => {
+    myProfile.value = res.value;
+  });
+  onUnmounted(unsubscribe);
+
+  const runWhenMyProfileExists = (fn: () => void) => {
+    if (!myProfile.value) {
+      $q.dialog({
+        component: CreateProfileDialog,
+      }).onOk((profile) => {
+        myProfile.value = profile;
+        fn();
+      });
+      return;
+    } else {
+      fn();
+    }
+  };
+
+  return { myProfile, runWhenMyProfileExists };
 };
