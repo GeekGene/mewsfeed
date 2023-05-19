9ee132e1f70618bca72981d10ac1b55e18a641a4
Matt Gabrenya <matt@buildyourweb.app>
2023-05-19 11:18:06 -0700

    refactor(ui): extract mini agent profile from EmbedMew into own component

---
+++ ui/src/components/BaseAgentProfile.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row justify-end items-center">
+    <div v-if="profile" class="row justify-center items-center">
+      <agent-avatar
+        :agentPubKey="agentPubKey"
+        size="20"
+        disable-tooltip
+        disable-copy
+        class="q-mr-xs"
+      />
+      <span class="q-mr-xs text-primary text-weight-bold">
+        {{ profile.fields[PROFILE_FIELDS.DISPLAY_NAME] }}
+      </span>
+      <span>@{{ profile.nickname }}</span>
+    </div>
+    <span v-else>
+      {{ encodeHashToBase64(agentPubKey).slice(0, 8) }}
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Profile } from "@holochain-open-dev/profiles";
+import { AgentPubKey, encodeHashToBase64 } from "@holochain/client";
+import { PROFILE_FIELDS } from "@/types/types";
+
+defineProps<{
+  agentPubKey: AgentPubKey;
+  profile?: Profile | null;
+}>();
+</script>
+
+<style scoped></style>
--- ui/src/components/BaseEmbedMew.vue
+++ ui/src/components/BaseEmbedMew.vue
@@ -1,36 +1,18 @@
 <template>
   <div class="q-mx-sm q-my-sm">
     <MewContent :mew="embedMew.mew as Mew" class="cursor-pointer q-mb-sm" />
-    <div class="row justify-end items-center">
-      <div
-        v-if="embedMew.author_profile"
-        class="row justify-center items-center"
-      >
-        <agent-avatar
-          :agentPubKey="embedMew.action.author"
-          size="20"
-          disable-tooltip
-          disable-copy
-          class="q-mr-xs"
-        />
-        <span class="q-mr-xs text-primary text-weight-bold">
-          {{ embedMew.author_profile.fields[PROFILE_FIELDS.DISPLAY_NAME] }}
-        </span>
-        <span>@{{ embedMew.author_profile.nickname }}</span>
-      </div>
-      <span v-else>
-        {{ encodeHashToBase64(embedMew.action.author).slice(0, 8) }}
-      </span>
-    </div>
+    <BaseAgentProfile
+      :agent-pub-key="embedMew.action.author"
+      :profile="embedMew.author_profile"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import { EmbedMew } from "@/types/types";
-import { encodeHashToBase64 } from "@holochain/client";
-import { PROFILE_FIELDS, Mew } from "@/types/types";
+import { Mew } from "@/types/types";
 import MewContent from "@/components/MewContent.vue";
-import ProfileAvatarWithPopup from "./ProfileAvatarWithPopup.vue";
+import BaseAgentProfile from "@/components/BaseAgentProfile.vue";
 
 defineProps<{
   embedMew: EmbedMew;
