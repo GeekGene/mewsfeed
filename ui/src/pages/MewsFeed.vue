a3d1f7b32809a812cb4e21e33b978e6895f30a69
Matt Gabrenya <matt@buildyourweb.app>
2023-05-24 18:44:54 -0700

    fix(ui): cache feed mews by agent key

--- ui/src/pages/MewsFeed.vue
+++ ui/src/pages/MewsFeed.vue
@@ -5,7 +5,9 @@
       :show-create-mew-field="true"
       :fetch-fn="fetchMewsFeed"
       title="Your Mews Feed"
-      :cache-key="`mews/get_my_followed_creators_mews_with_context`"
+      :cache-key="`mews/get_followed_creators_mews_with_context/${encodeHashToBase64(
+        client.myPubKey
+      )}`"
       :insert-responses="true"
     />
   </QPage>
@@ -14,7 +16,7 @@
 <script setup lang="ts">
 import { QPage } from "quasar";
 import { pageHeightCorrection } from "@/utils/page-layout";
-import { AppAgentClient } from "@holochain/client";
+import { AppAgentClient, encodeHashToBase64 } from "@holochain/client";
 import { ComputedRef, inject, watch, ref } from "vue";
 import { FeedMew } from "@/types/types";
 import MewList from "@/components/MewList.vue";
@@ -34,8 +36,8 @@ const fetchMewsFeed = (): Promise<FeedMew[]> =>
   client.callZome({
     role_name: "mewsfeed",
     zome_name: "mews",
-    fn_name: "get_my_followed_creators_mews_with_context",
-    payload: null,
+    fn_name: "get_followed_creators_mews_with_context",
+    payload: client.myPubKey,
   });
 
 const fetchFollowedCreators = (): Promise<FeedMew[]> =>
