207c63dc0712ebffb28eda89d62230efd8f8fd08
Matt Gabrenya <matt@buildyourweb.app>
2023-05-26 12:05:13 -0700

    feat(dna): add crate hc_link_pagination for keeping pagination logic, implement pagination by hash + limit

---
+++ crates/hc_link_pagination/src/hash_pagination.rs
@@ -0,0 +1,43 @@
+use hdk::prelude::*;
+
+#[derive(Serialize, Deserialize, SerializedBytes, Debug, Clone)]
+pub struct HashPagination {
+    pub after_hash: Option<AnyLinkableHash>,
+    pub limit: usize,
+}
+
+pub fn get_by_hash_pagination(
+    links: Vec<Link>,
+    page: Option<HashPagination>,
+) -> ExternResult<Vec<Link>> {
+    let mut links_copy = links.clone();
+    links_copy.sort_by_key(|l| l.timestamp);
+
+    match page {
+        Some(HashPagination { after_hash, limit }) => {
+            
+            let start_index = match after_hash {
+                Some(hash) => {
+                    match links_copy.iter().position(|l| l.target == hash) {
+                        Some(prev_position) => prev_position + 1,
+                        None => 0
+                    }
+                }
+                None => 0,
+            };
+
+            let maybe_slice = match limit <= links.len() {
+                true => links.get(start_index..start_index+limit),
+                false => links.get(start_index..links.len()),
+            };
+
+            match maybe_slice {
+                Some(slice) => {
+                    Ok(slice.to_vec())
+                }
+                None => Ok(vec![]),
+            }
+        }
+        None => Ok(links),
+    }
+}
---
+++ crates/hc_link_pagination/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod hash_pagination;
+pub use hash_pagination::*;
