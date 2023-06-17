a6dc457372f4e5762423b6c54dc83b1db0482006
Matt Gabrenya <matt@buildyourweb.app>
2023-06-16 22:27:05 -0700

    refactor(ui): have CreateProfileIfNotFound dialog use headlessui modal

--- ui/src/components/CreateProfileIfNotFoundDialog.vue
+++ ui/src/components/CreateProfileIfNotFoundDialog.vue
@@ -1,35 +1,73 @@
 <template>
-  <QDialog
-    :model-value="modelValue"
-    @update:model-value="(val: boolean) => emit('update:model-value', val)"
-    @hide="emit('update:model-value', false)"
-    @escape-key="emit('update:model-value', false)"
-  >
-    <QCard>
-      <QToolbar v-if="showCloseButton">
-        <QSpace />
-        <QBtn v-close-popup dense flat round icon="close" />
-      </QToolbar>
+  <TransitionRoot as="template" :show="modelValue">
+    <Dialog
+      as="div"
+      class="relative z-10"
+      @close="emit('update:model-value', false)"
+    >
+      <TransitionChild
+        as="template"
+        enter="ease-out duration-300"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="ease-in duration-200"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div
+          class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity backdrop-blur-sm"
+        ></div>
+      </TransitionChild>
 
-      <create-profile
-        v-if="profilesStore && !myProfile"
-        :store="profilesStore"
-        @profile-created="emit('profile-created')"
-      ></create-profile>
-      <slot v-else></slot>
-    </QCard>
-  </QDialog>
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0"
+        >
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
+            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leave-from="opacity-100 translate-y-0 sm:scale-100"
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <DialogPanel>
+              <create-profile
+                v-if="profilesStore && !myProfile"
+                :store="profilesStore"
+                class="font-content text-left prose"
+                style="
+                  .title {
+                    font-family: Syne;
+                  }
+                  text-align: left;
+                "
+                @profile-created="emit('profile-created')"
+              ></create-profile>
+              <slot v-else></slot>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
 
 <script setup lang="ts">
-import { QDialog, QCard, QToolbar, QBtn, QSpace } from "quasar";
 import { ComputedRef, inject } from "vue";
 import { Profile, ProfilesStore } from "@holochain-open-dev/profiles";
+import {
+  Dialog,
+  DialogPanel,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
 
 const profilesStore = (inject("profilesStore") as ComputedRef<ProfilesStore>)
   .value;
 const myProfile = inject("myProfile") as ComputedRef<Profile>;
-const emit = defineEmits(["profile-created", "update:model-value"]);
+const emit = defineEmits(["update:model-value", "profile-created"]);
 withDefaults(
   defineProps<{
     modelValue: boolean;
