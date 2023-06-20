41d9345b41ae7cc7292fa48f9f4ad11fca93cec1
Matt Gabrenya <matt@buildyourweb.app>
2023-06-19 19:10:39 -0700

    fix(ui): keep profile popup visible when hovering popup

--- ui/src/components/LinkProfilePopup.vue
+++ ui/src/components/LinkProfilePopup.vue
@@ -8,21 +8,25 @@
     "
     class="text-secondary text-bold relative inline-block overflow-visible"
     @click.stop
-    @mouseenter="isPopupVisible = true"
-    @mouseleave="isPopupVisible = false"
+    @mouseenter="setPopupVisible(true)"
+    @mouseleave="setPopupVisible(false)"
   >
     <div>
       <div><slot></slot></div>
     </div>
   </RouterLink>
-  <div v-show="isPopupVisible" class="absolute right-0 mt-12 w-full z-20">
+  <div v-show="isPopupVisible" class="absolute right-0 w-full z-20 mt-12">
     <div class="flex justify-center">
       <div class="w-full max-w-screen-lg flex items-center sm:space-x-10 px-8">
         <div class="block w-0 sm:w-14 h-14"></div>
         <div
           class="bg-neutral/5 backdrop-blur-md rounded-3xl shadow-xl flex-1 w-full"
         >
-          <ProfilePopupContent :agentPubKey="agentPubKey" />
+          <ProfilePopupContent
+            :agentPubKey="agentPubKey"
+            @mouseenter="setPopupVisible(true)"
+            @mouseleave="setPopupVisible(false)"
+          />
         </div>
       </div>
     </div>
@@ -35,6 +39,7 @@ import { encodeHashToBase64 } from "@holochain/client";
 import { ROUTES } from "@/router";
 import { RouteLocationRaw, RouterLink } from "vue-router";
 import ProfilePopupContent from "@/components/ProfilePopupContent.vue";
+import { debounce } from "lodash";
 
 defineProps<{
   to?: RouteLocationRaw;
@@ -42,4 +47,8 @@ defineProps<{
 }>();
 
 const isPopupVisible = ref<boolean>(false);
+
+const setPopupVisible = debounce((val: boolean) => {
+  isPopupVisible.value = val;
+}, 10);
 </script>
