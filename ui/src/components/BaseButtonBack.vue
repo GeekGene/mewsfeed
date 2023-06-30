7455f84100e77c08c10f3e981b42f51393803a24
Matt Gabrenya <matt@buildyourweb.app>
2023-06-30 11:40:01 -0700

    fix(ui): tighten back button

--- ui/src/components/BaseButtonBack.vue
+++ ui/src/components/BaseButtonBack.vue
@@ -1,5 +1,8 @@
 <template>
-  <button class="btn btn-ghost btn-md flex items-center" @click="router.back()">
+  <button
+    class="btn btn-ghost btn-md flex items-center px-2 py-0"
+    @click="router.back()"
+  >
     <IconArrowBack class="text-xl font-title" />
   </button>
 </template>
