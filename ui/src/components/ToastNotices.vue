bae7c25a8ce63e51c487b0eb5c224f74eaa13a1b
Matt Gabrenya <matt@buildyourweb.app>
2023-07-02 18:39:54 -0700

    feat(ui): add icons to toast notices

--- ui/src/components/ToastNotices.vue
+++ ui/src/components/ToastNotices.vue
@@ -3,7 +3,7 @@
     <div
       v-for="message in messages"
       :key="message.id"
-      class="alert"
+      class="alert flex justify-start items-center"
       :class="{
         'alert-info': message.type === 'info',
         'alert-success': message.type === 'success',
@@ -11,7 +11,11 @@
         'alert-warning': message.type === 'warning',
       }"
     >
-      {{ message.text }}
+      <IconInformationCircleOutline v-if="message.type === 'info'" />
+      <IconCheckmarkCircleOutline v-if="message.type === 'success'" />
+      <IconAlertCircleOutline v-if="message.type === 'error'" />
+      <IconWarningOutline v-if="message.type === 'warning'" />
+      <div>{{ message.text }}</div>
     </div>
   </div>
 </template>
@@ -19,6 +23,10 @@
 <script setup lang="ts">
 import { useToasts } from "@/stores/toasts";
 import { storeToRefs } from "pinia";
+import IconAlertCircleOutline from "~icons/ion/close-circle-outline";
+import IconInformationCircleOutline from "~icons/ion/information-circle-outline";
+import IconCheckmarkCircleOutline from "~icons/ion/checkmark-circle-outline";
+import IconWarningOutline from "~icons/ion/warning-outline";
 
 const { messages } = storeToRefs(useToasts());
 </script>
