795b6c2226035c29a311b8e13e38ab1f82ed2fd4
Matt Gabrenya <matt@buildyourweb.app>
2023-07-02 18:02:54 -0700

    fix(ui): ensure skeleton colors match site theme

--- ui/src/components/BaseAgentProfileListItemSkeleton.vue
+++ ui/src/components/BaseAgentProfileListItemSkeleton.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
     <div class="flex items-center mt-4 space-x-3 animate-pulse">
-      <IconPersonCircleOutline
-        class="text-base-300 fill-base-300 stroke-base-300 w-14 h-14 p-0 m-0"
-      />
+      <IconPersonCircle class="text-base-300 w-14 h-14 p-0 m-0" />
 
       <div>
         <div class="flex items-center w-full space-x-2 mb-2">
-          <div class="h-2.5 bg-gray-300 rounded-full w-32"></div>
-          <div class="h-2.5 bg-gray-200 rounded-full w-24"></div>
+          <div class="h-2.5 bg-base-300 rounded-full w-32"></div>
+          <div class="h-2.5 bg-base-200 rounded-full w-24"></div>
         </div>
 
         <div class="w-full flex flex-col space-y-1">
-          <div class="w-full h-2 bg-gray-100 rounded-full"></div>
-          <div class="w-full h-2 bg-gray-100 rounded-full"></div>
+          <div class="w-full h-2 bg-base-100 rounded-full"></div>
+          <div class="w-full h-2 bg-base-100 rounded-full"></div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import IconPersonCircleOutline from "~icons/ion/person-circle-outline";
+import IconPersonCircle from "~icons/ion/person-circle";
 </script>
--- ui/src/components/BaseMewListItemSkeleton.vue
+++ ui/src/components/BaseMewListItemSkeleton.vue
@@ -1,37 +1,38 @@
 <template>
   <div class="flex justify-start items-start space-x-2 px-2 animate-pulse">
-    <IconPersonCircleOutline
-      class="text-base-300 fill-base-300 stroke-base-300 w-14 h-14 p-0 m-0"
-    />
+    <IconPersonCircle class="text-base-300 w-14 h-14 p-0 m-0" />
     <div class="flex-1">
       <div class="flex justify-between items-center">
         <div class="flex items-center w-full space-x-2">
-          <div class="h-2.5 bg-gray-300 rounded-full w-32"></div>
-          <div class="h-2.5 bg-gray-200 rounded-full w-24"></div>
+          <div class="h-2.5 bg-base-300 rounded-full w-32"></div>
+          <div class="h-2.5 bg-base-200 rounded-full w-24"></div>
         </div>
-        <div class="h-2.5 bg-gray-200 rounded-full w-32"></div>
+        <div class="h-2.5 bg-base-200 rounded-full w-32"></div>
       </div>
 
       <div class="flex flex-col w-full space-y-2 my-4">
-        <div class="h-1.5 bg-gray-200 rounded-full w-5/6"></div>
-        <div class="h-1.5 bg-gray-200 rounded-full w-3/4"></div>
-        <div class="h-1.5 bg-gray-200 rounded-full w-5/6"></div>
+        <div class="h-1.5 bg-base-200 rounded-full w-5/6"></div>
+        <div class="h-1.5 bg-base-200 rounded-full w-3/4"></div>
+        <div class="h-1.5 bg-base-200 rounded-full w-5/6"></div>
+        <div class="h-1.5 bg-base-200 rounded-full w-3/4"></div>
       </div>
 
       <div class="flex justify-between items-center">
         <div class="flex justify-start items-center space-x-2">
-          <div class="bg-gray-200 rounded-full w-4 h-4"></div>
-          <div class="bg-gray-200 rounded-full w-4 h-4"></div>
-          <div class="bg-gray-200 rounded-full w-4 h-4"></div>
-          <div class="bg-gray-200 rounded-full w-4 h-4"></div>
+          <div class="bg-base-200 rounded-full w-4 h-4"></div>
+          <div class="bg-base-200 rounded-full w-4 h-4"></div>
+          <div class="bg-base-200 rounded-full w-4 h-4"></div>
+          <div class="bg-base-200 rounded-full w-4 h-4"></div>
         </div>
         <div class="flex justify-start items-center space-x-2">
-          <div class="bg-gray-200 rounded-full w-4 h-4"></div>
-          <div class="bg-gray-200 rounded-full w-4 h-4"></div>
+          <div class="bg-base-300 rounded-full w-4 h-4"></div>
+          <div class="bg-base-300 rounded-full w-4 h-4"></div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IconPersonCircle from "~icons/ion/person-circle";
+</script>
