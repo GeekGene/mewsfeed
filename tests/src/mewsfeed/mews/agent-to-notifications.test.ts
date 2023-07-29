eac3404cb95ebb83f3a39ab7f5d7fce4128be261
Matt Gabrenya <matt@buildyourweb.app>
2023-07-29 13:07:28 -0700

    test: extend timeout of slow tests

--- tests/src/mewsfeed/mews/agent-to-notifications.test.ts
+++ tests/src/mewsfeed/mews/agent-to-notifications.test.ts
@@ -444,272 +444,278 @@ test("notifications include replies, quotes, mewmews to mews that I also respond
   );
 });
 
-test("notifications list is time-paginated", async () => {
-  await runScenario(
-    async (scenario) => {
-      // Set up the app to be installed
-      const appSource = { appBundleSource: mewsfeedAppBundleSource };
+test(
+  "notifications list is time-paginated",
+  async () => {
+    await runScenario(
+      async (scenario) => {
+        // Set up the app to be installed
+        const appSource = { appBundleSource: mewsfeedAppBundleSource };
 
-      // Add 2 players with the test app to the Scenario. The returned players
-      // can be destructured.
-      const [alice, bob] = await scenario.addPlayersWithApps([
-        appSource,
-        appSource,
-      ]);
+        // Add 2 players with the test app to the Scenario. The returned players
+        // can be destructured.
+        const [alice, bob] = await scenario.addPlayersWithApps([
+          appSource,
+          appSource,
+        ]);
 
-      // Shortcut peer discovery through gossip and register all agents in every
-      // conductor of the scenario.
-      await scenario.shareAllAgents();
+        // Shortcut peer discovery through gossip and register all agents in every
+        // conductor of the scenario.
+        await scenario.shareAllAgents();
 
-      // Alice creates a Mew
-      const actionHash: ActionHash = await createMew(alice.cells[0]);
-      assert.ok(actionHash);
+        // Alice creates a Mew
+        const actionHash: ActionHash = await createMew(alice.cells[0]);
+        assert.ok(actionHash);
 
-      await dhtSync([alice, bob], alice.cells[0].cell_id[0]);
+        await dhtSync([alice, bob], alice.cells[0].cell_id[0]);
 
-      // Bob replies to Alice's mew
-      const replyInput: Mew = {
-        text: "xyxyxyxy test reply 1",
-        links: [],
-        mew_type: { [MewTypeName.Reply]: actionHash },
-      };
-      const replyActionHash: ActionHash = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "create_mew",
-        payload: replyInput,
-      });
-      const replyFeedMew: FeedMew = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "get_mew_with_context",
-        payload: replyActionHash,
-      });
+        // Bob replies to Alice's mew
+        const replyInput: Mew = {
+          text: "xyxyxyxy test reply 1",
+          links: [],
+          mew_type: { [MewTypeName.Reply]: actionHash },
+        };
+        const replyActionHash: ActionHash = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "create_mew",
+          payload: replyInput,
+        });
+        const replyFeedMew: FeedMew = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "get_mew_with_context",
+          payload: replyActionHash,
+        });
 
-      // Bob replies to Alice's mew
-      const replyInput2: Mew = {
-        text: "xyxyxyxy test reply 2",
-        links: [],
-        mew_type: { [MewTypeName.Reply]: actionHash },
-      };
-      const replyActionHash2: ActionHash = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "create_mew",
-        payload: replyInput2,
-      });
-      const replyFeedMew2: FeedMew = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "get_mew_with_context",
-        payload: replyActionHash2,
-      });
+        // Bob replies to Alice's mew
+        const replyInput2: Mew = {
+          text: "xyxyxyxy test reply 2",
+          links: [],
+          mew_type: { [MewTypeName.Reply]: actionHash },
+        };
+        const replyActionHash2: ActionHash = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "create_mew",
+          payload: replyInput2,
+        });
+        const replyFeedMew2: FeedMew = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "get_mew_with_context",
+          payload: replyActionHash2,
+        });
 
-      // Bob replies to Alice's mew
-      const replyInput3: Mew = {
-        text: "xyxyxyxy test reply 3",
-        links: [],
-        mew_type: { [MewTypeName.Reply]: actionHash },
-      };
-      const replyActionHash3: ActionHash = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "create_mew",
-        payload: replyInput3,
-      });
-      const replyFeedMew3: FeedMew = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "get_mew_with_context",
-        payload: replyActionHash3,
-      });
+        // Bob replies to Alice's mew
+        const replyInput3: Mew = {
+          text: "xyxyxyxy test reply 3",
+          links: [],
+          mew_type: { [MewTypeName.Reply]: actionHash },
+        };
+        const replyActionHash3: ActionHash = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "create_mew",
+          payload: replyInput3,
+        });
+        const replyFeedMew3: FeedMew = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "get_mew_with_context",
+          payload: replyActionHash3,
+        });
 
-      // Bob replies to Alice's mew
-      const replyInput4: Mew = {
-        text: "xyxyxyxy test reply 4",
-        links: [],
-        mew_type: { [MewTypeName.Reply]: actionHash },
-      };
-      const replyActionHash4: ActionHash = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "create_mew",
-        payload: replyInput4,
-      });
-      const replyFeedMew4: FeedMew = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "get_mew_with_context",
-        payload: replyActionHash4,
-      });
+        // Bob replies to Alice's mew
+        const replyInput4: Mew = {
+          text: "xyxyxyxy test reply 4",
+          links: [],
+          mew_type: { [MewTypeName.Reply]: actionHash },
+        };
+        const replyActionHash4: ActionHash = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "create_mew",
+          payload: replyInput4,
+        });
+        const replyFeedMew4: FeedMew = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "get_mew_with_context",
+          payload: replyActionHash4,
+        });
 
-      // Bob replies to Alice's mew
-      const replyInput5: Mew = {
-        text: "xyxyxyxy test reply 5",
-        links: [],
-        mew_type: { [MewTypeName.Reply]: actionHash },
-      };
-      const replyActionHash5: ActionHash = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "create_mew",
-        payload: replyInput5,
-      });
-      const replyFeedMew5: FeedMew = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "get_mew_with_context",
-        payload: replyActionHash5,
-      });
+        // Bob replies to Alice's mew
+        const replyInput5: Mew = {
+          text: "xyxyxyxy test reply 5",
+          links: [],
+          mew_type: { [MewTypeName.Reply]: actionHash },
+        };
+        const replyActionHash5: ActionHash = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "create_mew",
+          payload: replyInput5,
+        });
+        const replyFeedMew5: FeedMew = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "get_mew_with_context",
+          payload: replyActionHash5,
+        });
 
-      // Bob replies to Alice's mew
-      const replyInput6: Mew = {
-        text: "xyxyxyxy test reply 6",
-        links: [],
-        mew_type: { [MewTypeName.Reply]: actionHash },
-      };
-      const replyActionHash6: ActionHash = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "create_mew",
-        payload: replyInput6,
-      });
-      const replyFeedMew6: FeedMew = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "get_mew_with_context",
-        payload: replyActionHash6,
-      });
+        // Bob replies to Alice's mew
+        const replyInput6: Mew = {
+          text: "xyxyxyxy test reply 6",
+          links: [],
+          mew_type: { [MewTypeName.Reply]: actionHash },
+        };
+        const replyActionHash6: ActionHash = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "create_mew",
+          payload: replyInput6,
+        });
+        const replyFeedMew6: FeedMew = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "get_mew_with_context",
+          payload: replyActionHash6,
+        });
 
-      // Bob replies to Alice's mew
-      const replyInput7: Mew = {
-        text: "xyxyxyxy test reply 7",
-        links: [],
-        mew_type: { [MewTypeName.Reply]: actionHash },
-      };
-      const replyActionHash7: ActionHash = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "create_mew",
-        payload: replyInput7,
-      });
-      const replyFeedMew7: FeedMew = await bob.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "get_mew_with_context",
-        payload: replyActionHash7,
-      });
+        // Bob replies to Alice's mew
+        const replyInput7: Mew = {
+          text: "xyxyxyxy test reply 7",
+          links: [],
+          mew_type: { [MewTypeName.Reply]: actionHash },
+        };
+        const replyActionHash7: ActionHash = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "create_mew",
+          payload: replyInput7,
+        });
+        const replyFeedMew7: FeedMew = await bob.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "get_mew_with_context",
+          payload: replyActionHash7,
+        });
 
-      await dhtSync([alice, bob], alice.cells[0].cell_id[0]);
+        await dhtSync([alice, bob], alice.cells[0].cell_id[0]);
 
-      // Alice gets notifications
-      const page1 = await alice.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "get_my_notifications",
-        payload: {
-          direction: { [PaginationDirectionName.Descending]: null },
-          limit: 2,
-        },
-      });
+        // Alice gets notifications
+        const page1 = await alice.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "get_my_notifications",
+          payload: {
+            direction: { [PaginationDirectionName.Descending]: null },
+            limit: 2,
+          },
+        });
 
-      // Notifications should be orderded by action time descending (newest first)
-      expect(page1[0]).toMatchObject({
-        agent: bob.agentPubKey,
-        notification_type: { [NotificationTypeName.MyMewResponded]: null },
-        feed_mew: replyFeedMew7,
-      });
-      expect(page1[1]).toMatchObject({
-        agent: bob.agentPubKey,
-        notification_type: { [NotificationTypeName.MyMewResponded]: null },
-        feed_mew: replyFeedMew6,
-      });
+        // Notifications should be orderded by action time descending (newest first)
+        expect(page1[0]).toMatchObject({
+          agent: bob.agentPubKey,
+          notification_type: { [NotificationTypeName.MyMewResponded]: null },
+          feed_mew: replyFeedMew7,
+        });
+        expect(page1[1]).toMatchObject({
+          agent: bob.agentPubKey,
+          notification_type: { [NotificationTypeName.MyMewResponded]: null },
+          feed_mew: replyFeedMew6,
+        });
 
-      expect(page1[0].timestamp).greaterThanOrEqual(page1[1].timestamp);
+        expect(page1[0].timestamp).greaterThanOrEqual(page1[1].timestamp);
 
-      // Alice gets notifications
-      const page2 = await alice.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "get_my_notifications",
-        payload: {
-          after_timestamp: page1[1].timestamp,
-          direction: { [PaginationDirectionName.Descending]: null },
-          limit: 2,
-        },
-      });
+        // Alice gets notifications
+        const page2 = await alice.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "get_my_notifications",
+          payload: {
+            after_timestamp: page1[1].timestamp,
+            direction: { [PaginationDirectionName.Descending]: null },
+            limit: 2,
+          },
+        });
 
-      // Notifications should be orderded by action time descending (newest first)
-      expect(page2[0]).toMatchObject({
-        agent: bob.agentPubKey,
-        notification_type: { [NotificationTypeName.MyMewResponded]: null },
-        feed_mew: replyFeedMew5,
-      });
-      expect(page2[1]).toMatchObject({
-        agent: bob.agentPubKey,
-        notification_type: { [NotificationTypeName.MyMewResponded]: null },
-        feed_mew: replyFeedMew4,
-      });
+        // Notifications should be orderded by action time descending (newest first)
+        expect(page2[0]).toMatchObject({
+          agent: bob.agentPubKey,
+          notification_type: { [NotificationTypeName.MyMewResponded]: null },
+          feed_mew: replyFeedMew5,
+        });
+        expect(page2[1]).toMatchObject({
+          agent: bob.agentPubKey,
+          notification_type: { [NotificationTypeName.MyMewResponded]: null },
+          feed_mew: replyFeedMew4,
+        });
 
-      expect(page1[0].timestamp)
-        .greaterThanOrEqual(page1[1].timestamp)
-        .greaterThanOrEqual(page2[0].timestamp)
-        .greaterThanOrEqual(page2[1].timestamp);
+        expect(page1[0].timestamp)
+          .greaterThanOrEqual(page1[1].timestamp)
+          .greaterThanOrEqual(page2[0].timestamp)
+          .greaterThanOrEqual(page2[1].timestamp);
 
-      // Alice gets notifications
-      const page3 = await alice.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "get_my_notifications",
-        payload: {
-          after_timestamp: page2[1].timestamp,
-          direction: { [PaginationDirectionName.Descending]: null },
-          limit: 2,
-        },
-      });
+        // Alice gets notifications
+        const page3 = await alice.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "get_my_notifications",
+          payload: {
+            after_timestamp: page2[1].timestamp,
+            direction: { [PaginationDirectionName.Descending]: null },
+            limit: 2,
+          },
+        });
 
-      // Notifications should be orderded by action time descending (newest first)
-      expect(page3[0]).toMatchObject({
-        agent: bob.agentPubKey,
-        notification_type: { [NotificationTypeName.MyMewResponded]: null },
-        feed_mew: replyFeedMew3,
-      });
-      expect(page3[1]).toMatchObject({
-        agent: bob.agentPubKey,
-        notification_type: { [NotificationTypeName.MyMewResponded]: null },
-        feed_mew: replyFeedMew2,
-      });
+        // Notifications should be orderded by action time descending (newest first)
+        expect(page3[0]).toMatchObject({
+          agent: bob.agentPubKey,
+          notification_type: { [NotificationTypeName.MyMewResponded]: null },
+          feed_mew: replyFeedMew3,
+        });
+        expect(page3[1]).toMatchObject({
+          agent: bob.agentPubKey,
+          notification_type: { [NotificationTypeName.MyMewResponded]: null },
+          feed_mew: replyFeedMew2,
+        });
 
-      expect(page1[0].timestamp)
-        .greaterThanOrEqual(page1[1].timestamp)
-        .greaterThanOrEqual(page2[0].timestamp)
-        .greaterThanOrEqual(page2[1].timestamp)
-        .greaterThanOrEqual(page3[0].timestamp)
-        .greaterThanOrEqual(page3[1].timestamp);
+        expect(page1[0].timestamp)
+          .greaterThanOrEqual(page1[1].timestamp)
+          .greaterThanOrEqual(page2[0].timestamp)
+          .greaterThanOrEqual(page2[1].timestamp)
+          .greaterThanOrEqual(page3[0].timestamp)
+          .greaterThanOrEqual(page3[1].timestamp);
 
-      // Alice gets notifications
-      const page4 = await alice.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "get_my_notifications",
-        payload: {
-          after_timestamp: page3[1].timestamp,
-          direction: { [PaginationDirectionName.Descending]: null },
-          limit: 2,
-        },
-      });
+        // Alice gets notifications
+        const page4 = await alice.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "get_my_notifications",
+          payload: {
+            after_timestamp: page3[1].timestamp,
+            direction: { [PaginationDirectionName.Descending]: null },
+            limit: 2,
+          },
+        });
 
-      // Notifications should be orderded by action time descending (newest first)
-      assert.lengthOf(page4, 1);
-      expect(page4[0]).toMatchObject({
-        agent: bob.agentPubKey,
-        notification_type: { [NotificationTypeName.MyMewResponded]: null },
-        feed_mew: replyFeedMew,
-      });
+        // Notifications should be orderded by action time descending (newest first)
+        assert.lengthOf(page4, 1);
+        expect(page4[0]).toMatchObject({
+          agent: bob.agentPubKey,
+          notification_type: { [NotificationTypeName.MyMewResponded]: null },
+          feed_mew: replyFeedMew,
+        });
 
-      expect(page1[0].timestamp)
-        .greaterThanOrEqual(page1[1].timestamp)
-        .greaterThanOrEqual(page2[0].timestamp)
-        .greaterThanOrEqual(page2[1].timestamp)
-        .greaterThanOrEqual(page3[0].timestamp)
-        .greaterThanOrEqual(page3[1].timestamp)
-        .greaterThanOrEqual(page4[0].timestamp);
+        expect(page1[0].timestamp)
+          .greaterThanOrEqual(page1[1].timestamp)
+          .greaterThanOrEqual(page2[0].timestamp)
+          .greaterThanOrEqual(page2[1].timestamp)
+          .greaterThanOrEqual(page3[0].timestamp)
+          .greaterThanOrEqual(page3[1].timestamp)
+          .greaterThanOrEqual(page4[0].timestamp);
 
-      const page5 = await alice.cells[0].callZome({
-        zome_name: "mews",
-        fn_name: "get_my_notifications",
-        payload: {
-          after_timestamp: page4[0].timestamp,
-          direction: { [PaginationDirectionName.Descending]: null },
-          limit: 2,
-        },
-      });
+        const page5 = await alice.cells[0].callZome({
+          zome_name: "mews",
+          fn_name: "get_my_notifications",
+          payload: {
+            after_timestamp: page4[0].timestamp,
+            direction: { [PaginationDirectionName.Descending]: null },
+            limit: 2,
+          },
+        });
 
-      assert.lengthOf(page5, 0);
-    },
-    true,
-    { timeout: 150000 }
-  );
-});
+        assert.lengthOf(page5, 0);
+      },
+      true,
+      { timeout: 500000 }
+    );
+  },
+  {
+    timeout: 5000000,
+  }
+);
