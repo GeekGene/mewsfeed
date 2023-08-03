ccaa6f50f384e610e01ff52bb99da1b8be4f5936
Matt Gabrenya <matt@buildyourweb.app>
2023-08-03 14:02:54 -0700

    fix(ui): positioning of agent profile popup

--- ui/src/components/BaseLinkProfilePopup.vue
+++ ui/src/components/BaseLinkProfilePopup.vue
@@ -21,9 +21,8 @@
   >
     <div class="flex justify-center">
       <div
-        class="w-full max-w-screen-md flex justify-start items-center sm:space-x-10 px-2 sm:px-8"
+        class="w-full max-w-screen-md flex justify-start items-center sm:space-x-10"
       >
-        <div class="block w-0 sm:w-14 h-14 flex-none"></div>
         <div
           class="bg-neutral/5 backdrop-blur-md rounded-3xl flex-initial"
           style="-webkit-backdrop-filter: blur(10px)"
