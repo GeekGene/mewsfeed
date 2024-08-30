cd320bb19da4c2c8a6ba20e9e4463d48736f02ea
Matt Gabrenya <matt@buildyourweb.app>
2024-08-30 14:44:03 -0700

    build: switch to latest holonix, p2p-shipyard fork on latest holonix

--- flake.nix
+++ flake.nix
@@ -1,45 +1,64 @@
 {
-  description = "Template for Holochain app development";
+  description = "Flake for Holochain app development";
 
   inputs = {
-    p2p-shipyard.url = "github:darksoil-studio/p2p-shipyard";
-    versions.url  = "github:holochain/holochain?dir=versions/0_3";
+    holonix.url = "github:holochain/holonix?ref=main-0.3";
+    p2p-shipyard.url = "github:mattyg/p2p-shipyard?ref=refactor/switch-to-new-holonix";
 
-    holochain-flake.url = "github:holochain/holochain";
-    holochain-flake.inputs.versions.follows = "versions";
-
-    nixpkgs.follows = "holochain-flake/nixpkgs";
-    flake-parts.follows = "holochain-flake/flake-parts";
+    nixpkgs.follows = "holonix/nixpkgs";
+    flake-parts.follows = "holonix/flake-parts";
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake
-      {
-        inherit inputs;
-      }
-      {
-        systems = builtins.attrNames inputs.holochain-flake.devShells;
-        perSystem = 
-          { config
-          , pkgs
-          , system
-          , inputs'
-          , ... 
-          }: {
-            devShells.default = pkgs.mkShell {
-              inputsFrom = [
-                inputs'.p2p-shipyard.devShells.holochainTauriDev
-                inputs'.holochain-flake.devShells.holonix
-              ];
-              packages = [ pkgs.nodejs-18_x ];
-            };
-            devShells.androidDev = pkgs.mkShell {
-              inputsFrom = [
-                inputs'.p2p-shipyard.devShells.holochainTauriAndroidDev
-                inputs'.holochain-flake.devShells.holonix
-              ];
-              packages = [ pkgs.nodejs-18_x ];
-            };
-          };
+  outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = builtins.attrNames inputs.holonix.devShells;
+    perSystem = { inputs', pkgs, ... }: {
+      formatter = pkgs.nixpkgs-fmt;
+
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [ 
+          inputs'.holonix.devShells
+          inputs'.p2p-shipyard.devShells.holochainTauriDev
+        ];
+
+        packages = (with inputs'.holonix.packages; [
+          holochain
+          lair-keystore
+          hc-launch
+          hc-scaffold
+          hn-introspect
+        ]) ++ (with pkgs; [
+          nodejs_20 # For UI development
+          binaryen # For WASM optimisation
+          # Add any other packages you need here
+        ]);
+
+        shellHook = ''
+          export PS1='\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
+        '';
       };
+
+      devShells.androidDev = pkgs.mkShell {
+        inputsFrom = [
+          inputs'.holonix.devShells
+          inputs'.p2p-shipyard.devShells.holochainTauriAndroidDev
+        ];
+        
+        packages = (with inputs'.holonix.packages; [
+          holochain
+          lair-keystore
+          hc-launch
+          hc-scaffold
+          hn-introspect
+        ]) ++ (with pkgs; [
+          nodejs_20 # For UI development
+          binaryen # For WASM optimisation
+          # Add any other packages you need here
+        ]);
+
+        shellHook = ''
+          export PS1='\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
+        '';
+      };
+    };
+  };
 }
