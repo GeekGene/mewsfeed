0630b6c51ef5190ceae6b95530a40d19bda37ea9
Eric Harris-Braun <eric@harris-braun.com>
2026-03-06 22:23:15 -0500

    feat(ui): add cat logo next to mewsfeed title

--- ui/src/pages/MewsFeed.vue
+++ ui/src/pages/MewsFeed.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="w-full">
-    <h1 class="text-2xl font-title font-bold tracking-tighter mb-8 px-4">
+    <h1 class="text-2xl font-title font-bold tracking-tighter mb-8 px-4 flex items-center gap-2">
+      <img src="/mewsfeed_logo.png" alt="mewsfeed logo" class="w-8 h-8 -scale-x-100" />
       mewsfeed
     </h1>
 
